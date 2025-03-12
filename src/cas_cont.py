bc458a84199dd803a19afadf9461e4aa39b39dd7
Regalado <lregaladofloriano@gmail.com>
2025-03-12 07:42:28 -0600

    added basic connection

--- src/cas_cont.py
+++ src/cas_cont.py
@@ -1,2 +1,25 @@
+import logging
+import os
+
+from cassandra.cluster import Cluster
+
+def get_session(keyspace):
+
+    CLUSTER_IPS = os.getenv('CASSANDRA_CLUSTER_IPS', 'localhost')
+    KEYSPACE = os.getenv('CASSANDRA_KEYSPACE', keyspace)
+    REPLICATION_FACTOR = os.getenv('CASSANDRA_REPLICATION_FACTOR', '1')
+
+
+    cluster = Cluster(CLUSTER_IPS.split(','))
+    session = cluster.connect()
+
+    model.create_keyspace(session, KEYSPACE, REPLICATION_FACTOR)
+    session.set_keyspace(KEYSPACE)
+
+    model.create_schema(session)
+
+def main():
+    ...
+    
 if __name__ == "__main__":
     main()
