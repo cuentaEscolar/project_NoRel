fda48b0f6696d7079d8656d3c1913d43cf494327
Alan Solorio <69700672+AlanElPlatano@users.noreply.github.com>
2025-04-25 07:28:18 -0600

    Dgraph data loading complete
    If the CSV files exist, the data is now successfully sent to Dgraph

--- dataGen/loader.py
+++ dataGen/loader.py
@@ -1,7 +1,9 @@
 from Conexion import cassandra_model
 from Conexion import mongo_model
-from Conexion.dgraph_connection import DgraphConnection
+from Conexion.dgraph_connection import DgraphConnection #Conexion a Dgraph
+from Conexion.dgraph_loader import load_data_to_dgraph # Carga de datos a Dgraph
 from dataGen import generator
+import os
 
 if __name__ == "__main__":
     print("hi")
@@ -13,5 +15,9 @@ if __name__ == "__main__":
     #mongo_model.test_session(mongo_session)
     print(type(mongo_session))
 
-    # Inicializar Dgraph
     dgraph_client = DgraphConnection.initialize_dgraph()
+        
+    # Como la función load_data_to_dgraph espera recibir la carpeta donde están los csv
+    # Era más fácil pasarle la carpeta con 'os' que reescribir la función para asumir que es la carpeta actual
+    data_gen_path = os.path.dirname(os.path.abspath(__file__))
+    uids = load_data_to_dgraph(data_gen_path, None)
